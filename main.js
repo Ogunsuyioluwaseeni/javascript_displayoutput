78a7a39fca546ad850e2f1babd5c1cba86cf057b
Ogunsuyioluwaseeni <olamy2292@gmail.com>
2022-06-23 12:40:49 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,4 @@
+let myName = "Oluwaseeni Ogunsuyi";
+let myHeight = "5.5ft";
+let myCountry = "Nigeria";
+document.write("My name is :" + " " + myName + "<br>" + "My height is:" + " " + myHeight + "<br>" + "My Country is:" + " " + myCountry)
